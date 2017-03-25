84a50b4aca9e0876d37120ff837fa0b7821f897a
RijaliRahman <RijaliRahman17@gmail.com>
2017-03-25 20:50:29 +0800

    Posttest 4

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -5,33 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\pengguna;
+use App\Pengguna;
 
-class PenggunaController extends Controller
+class penggunaController extends Controller
 {
     public function awal()
     {
-    	return "Hello dari PenggunaController";
+        return view('pengguna.awal', ['data'=>pengguna::all()]);
     }
     public function tambah()
     {
-    	return 	$this -> simpan();
+        return view('pengguna.tambah');
     }
-    public function simpan()
+    public function simpan(Request $input)
     {
-    	$pengguna = new pengguna();
-    	$pengguna->username='rijali rahman';
-    	$pengguna->password='gagah';
-    	$pengguna->save();
-    	return "data dengan username {$pengguna->username}telah di simpan";
+      $pengguna = new pengguna;
+      $pengguna->username = $input->username;
+      $pengguna->password = $input->password;
+      $informasi = $pengguna->save() ? 'Berhasil simpan data' : 'Gagal simpan data';
+      return redirect('pengguna')->with(['informasi'=>$informasi]);
     }
-    
-    public function ayam()
+    public function edit($id)
     {
-    	$pengguna = new pengguna();
-    	$pengguna->username='muhammad rijali rahman';
-    	$pengguna->password='gagah';
-    	$pengguna->save();
-    	return "data dengan username {$pengguna->username} telah di simpan";
+      $pengguna = pengguna::find($id);
+      return view('pengguna.edit')->with(array('pengguna'=>$pengguna));
     }
-}
+    public function lihat($id)
+    {
+      $pengguna = pengguna::find($id);
+      return view('pengguna.lihat')->with(array('pengguna'=>$pengguna));
+    }
+    public function update($id, Request $input)
+    {
+      $pengguna = pengguna::find($id);
+      $pengguna->username = $input->username;
+      $pengguna->password = $input->password;
+      $informasi = $pengguna->save() ? 'Berhasil update data' :'Gagal update data';
+      return redirect('pengguna')->with(['informasi'=>$informasi]);
+    }
+    public function hapus($id)
+    {
+      $pengguna = pengguna::find($id);
+      $informasi = $pengguna->delete() ? 'Berhasil hapus data' : 'Gagal hapus data';
+      return redirect('pengguna')->with(['informasi'=>$informasi]);
+    } 
+  }
